a629ed9ea61f5b968fa09dac49de2cb75cc3c1a3
Fernando Monera <fmonera@gmail.com>
2023-05-20 01:48:00 +0200

    chart

---
+++ chart/package-push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PACKAGE=$1
+
+if [ "X$1" == "X" ]
+then
+	echo "Specify the package name as a parameter."
+	exit
+fi
+
+cd $PACKAGE
+helm dependency update
+cd ..
+
+echo "Packaging ..."
+rm -f *tgz
+helm package $PACKAGE
+
+P="$(ls *tgz)"
+
+if [ "X$P" == "X" ]
+then
+	echo "Error generating package."
+	exit 1
+fi
+
+echo "Pushing package $P ..."
+
+helm cm-push --username=fmonera $P wallstreetlocos
+helm repo update
+
